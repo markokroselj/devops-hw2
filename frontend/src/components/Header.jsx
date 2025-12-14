e88f12689fce9c019407149615c212a1407e3abc
markokroselj <kroselj.marko123@gmail.com>
2025-12-14 20:59:36 +0100

    Add React frontend

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='bg-slate-900 text-white px-2 py-4'>
+        <h1 className='text-xl'>Vehicle inventory manager</h1> 
+        </div>
+  )
+}
+ 
